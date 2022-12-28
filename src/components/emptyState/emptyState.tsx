4cc66597275352a3157827b7116fe70265306401
Michał <michaldziuba.praca@gmail.com>
2022-12-28 19:08:20 +0100

    Create emptyState.tsx

---
+++ src/components/emptyState/emptyState.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import image from '../../images/empty.png'
+const EmptyState:FC = () => {
+    return (
+        <div className='flex flex-col justify-center items-center mt-8  '>
+            <img src={image} alt='empty state' className='w-[290px] h-[320px] rounded-2xl'></img>
+        <p className='font-Montserrat text-xl font-medium mt-10 text-center'>"Hmmm... that orange button must be important..."</p>
+      </div>
+    );
+}
+export default EmptyState
